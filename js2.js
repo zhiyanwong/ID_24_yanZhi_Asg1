68370936885e387fc32b23ab4ec51d0d27787b86
Wong Yan Zhi <s10247154@connect.np.edu.sg>
2022-12-02 08:31:48 +0800

    hahah

---
+++ js2.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      singleItem: true,
+      itemsScaleUp : true,
+      slideSpeed: 500,
+      autoPlay: 5000,
+      stopOnHover: true
+    });
+  });
